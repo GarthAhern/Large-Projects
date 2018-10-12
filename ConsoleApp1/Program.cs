a0c20885026294ede08cdd5283bcb6fbd9fa85fc
GarthAhern <aherng@outlook.com>
2018-10-12 14:12:40 -0700

    //Exercise p. 66

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -6,6 +6,21 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
+            //Exercise p. 66
+            Console.WriteLine("Please give me a number: ");
+            decimal x = Convert.ToDecimal(Console.ReadLine());
+            Console.WriteLine("Your number multiplied by 50 is: " + (x * 50));
+            Console.WriteLine("Your number multiplied plus 25 is: " + (x + 25));
+
+            Console.WriteLine("Your number divided by 12.5 is: " + (x / 12.5m));
+            Console.WriteLine("The remainder of your number after dividing by 7 is: " + (x % 7));
+            Console.ReadLine();
+
+
+
+
+
+
             //console.writeline("what is your name?");
             //string yourname = console.readline();
             //console.writeline("your name is: " + yourname);
@@ -17,7 +32,7 @@ namespace ConsoleApp1
             //Console.WriteLine("Your favorite number is : " + favoriteNumber);
             //Console.ReadLine();
 
-           
+            //7
             //bool isStudying = false;
             //byte hoursWorked = 42;
             //sbyte currentTemp = -23;
@@ -28,24 +43,24 @@ namespace ConsoleApp1
             //float secondsLeft = 2.62f;
 
 
-            Console.WriteLine("The Tech Academy \n Student Daily Report \n What course are you on?");
-            string currentCourse = Console.ReadLine();
-            Console.WriteLine("What page number?");
-            int pNum = Convert.ToInt32(Console.ReadLine());
-            string backupPNum = pNum.ToString();
-            Console.WriteLine("Do you need help with anything? Please answer" +
-                " 'true' or 'false'");
-            bool needsHelp = Convert.ToBoolean(Console.ReadLine());
-            Console.WriteLine("Were there any positive experiences you’d like to share? Please give specifics.");
-            string posExperience = Console.ReadLine();
-            Console.WriteLine("Is there any other feedback you’d like to provide? Please be specific.");
-            string feedback = Console.ReadLine();
-            Console.WriteLine("How many hours did you study today?");
-            float hours = float.Parse(Console.ReadLine());
-            Console.WriteLine("Thank you for your answers. An Instructor will respond to this shortly. Have a great day!");
-            Console.WriteLine("Course on: " + currentCourse + "\nPage Number: " + pNum + "\nNeeds help: " + needsHelp + "\nExperiences: " + posExperience
-               + "\nFeedback: " + feedback + "\nHours studied: " + hours);
-            Console.ReadLine();
+            //Console.WriteLine("The Tech Academy \n Student Daily Report \n What course are you on?");
+            //string currentCourse = Console.ReadLine();
+            //Console.WriteLine("What page number?");
+            //int pNum = Convert.ToInt32(Console.ReadLine());
+            //string backupPNum = pNum.ToString();
+            //Console.WriteLine("Do you need help with anything? Please answer" +
+            //    " 'true' or 'false'");
+            //bool needsHelp = Convert.ToBoolean(Console.ReadLine());
+            //Console.WriteLine("Were there any positive experiences you’d like to share? Please give specifics.");
+            //string posExperience = Console.ReadLine();
+            //Console.WriteLine("Is there any other feedback you’d like to provide? Please be specific.");
+            //string feedback = Console.ReadLine();
+            //Console.WriteLine("How many hours did you study today?");
+            //float hours = float.Parse(Console.ReadLine());
+            //Console.WriteLine("Thank you for your answers. An Instructor will respond to this shortly. Have a great day!");
+            //Console.WriteLine("Course on: " + currentCourse + "\nPage Number: " + pNum + "\nNeeds help: " + needsHelp + "\nExperiences: " + posExperience
+            //   + "\nFeedback: " + feedback + "\nHours studied: " + hours);
+            //Console.ReadLine();
 
 
 
