ee71fb60a4a6ebae62f1e0bc3a1fdd142f9067d5
GarthAhern <aherng@outlook.com>
2018-10-15 14:51:12 -0700

    Added new project for Exercise p.100

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Text;
+using System.Collections.Generic;
 
 namespace ConsoleApp2
 {
@@ -7,9 +8,43 @@ namespace ConsoleApp2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
 
-            StringBuilder sb = new StringBuilder();
+
+            List<int> intList = new List<int>();
+
+            intList.Add(10);
+            intList.Add(11);
+            intList.Add(12);
+
+            Console.WriteLine(intList[0]);
+
+
+
+
+
+
+
+
+
+            //int[] numArray = new int[15];
+            //numArray[0] = 5;
+            //numArray[1] = 10;
+
+            //numArray[2] = 42;
+
+            //numArray[3] = 55;
+            //numArray[4] = 66;
+            //numArray[5] = 77;
+            //numArray[6] = 88;
+            //Console.WriteLine(numArray[0]);
+            //Console.ReadLine();
+
+            //int[] numar = new int[] { 5, 6, 4, 3, 2, 5, 76, 77, 4, 3, 36, 7 };
+
+
+            //int[] narr = { 4, 74, 35, 67, 3, 5, 6, 7, 99 };
+
+
         }
     }
 }
---
+++ Page 100 Exercise Lists and Arrays/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Page_100_Exercise_Lists_and_Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string[] a = new string[] { "up", "down", "under", "between", "below", "beneath", "above", "through"};
+            Console.WriteLine("Please select an index of the array between 0 and seven");
+            int x = Convert.ToInt16(Console.ReadLine());
+            Console.WriteLine(a[x]);
+            Console.ReadLine();
+
+
+
+            int[] iarry = new int[] {10,9,8,7,6,5,4,3,2,1,0};
+            Console.WriteLine("Please select an index of the array");
+            x = Convert.ToInt16(Console.ReadLine());
+            Console.WriteLine(iarry[x]);
+            Console.ReadLine();
+
+
+
+
+            List<string> strList = new List<string> {"Bob","Marty","Joe",
+                "Shirley","Harold","Haley","Mike","Jan"};
+            Console.WriteLine("Please select an index of the array between 0 and seven");
+            x = Convert.ToInt16(Console.ReadLine());
+            Console.WriteLine(strList[x]);
+            Console.ReadLine();
+
+            
+
+
+        }
+    }
+}
