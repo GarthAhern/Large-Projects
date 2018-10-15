885bd68ecd9ad08e352cb5519b93a75023f797ee
GarthAhern <aherng@outlook.com>
2018-10-15 13:04:36 -0700

    Page 93 Exercise with While and Do-While Loops.

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -6,7 +6,110 @@ namespace ConsoleApp2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int x = 0;
+            bool speed = false;
+            while (speed == false)
+            {
+                switch (x)
+                {
+
+                    case 1:
+
+                        Console.WriteLine("Sorry, but no.");
+                        break;
+
+
+                    case 2:
+                        Console.WriteLine("Sorry, but no.");
+                        break;
+
+                    case 3:
+                        Console.WriteLine("Sorry, but no.");
+                        break;
+                    case 4:
+                        Console.WriteLine("Sorry, but no.");
+                        break;
+                    case 5:
+                        Console.WriteLine("Sorry, but no.");
+                        break;
+                    case 6:
+                        Console.WriteLine("Sorry, but no.");
+                        break;
+                    case 7:
+                        Console.WriteLine("Sorry, but no.");
+                        break;
+
+                    case 8:
+                        Console.WriteLine("Sorry, but no.");
+                        break;
+                    case 9:
+                        Console.WriteLine("Congrats.");
+                        speed = true;
+                        break;
+
+                    default:
+                        Console.WriteLine("You are wrong");
+
+                        break;
+                }
+                x++;
+            }
+            Console.ReadLine();
+
+
+
+            int y = 0;
+            bool choice = true;
+
+            do
+            {
+                switch (y)
+                {
+
+                    case 1:
+
+                        Console.WriteLine("Sorry, but no.");
+                        break;
+
+
+                    case 2:
+                        Console.WriteLine("Sorry, but no.");
+                        break;
+
+                    case 3:
+                        Console.WriteLine("Sorry, but no.");
+                        break;
+                    case 4:
+                        Console.WriteLine("Sorry, but no.");
+                        break;
+                    case 5:
+                        Console.WriteLine("Sorry, but no.");
+                        break;
+                    case 6:
+                        Console.WriteLine("Sorry, but no.");
+                        break;
+                    case 7:
+                        Console.WriteLine("Sorry, but no.");
+                        break;
+
+                    case 8:
+                        Console.WriteLine("Sorry, but no.");
+                        break;
+                    case 9:
+                        Console.WriteLine("Congrats.");
+
+                        break;
+
+                    default:
+                        Console.WriteLine("You are wrong");
+                        choice = true;
+                        break;
+                }
+            }
+            while (choice == false);
+            Console.ReadLine();
+
+
         }
     }
 }
