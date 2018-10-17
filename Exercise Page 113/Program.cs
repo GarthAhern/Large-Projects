edfcf15d8e70a4544d308f69231fe3f489ecdf5d
GarthAhern <aherng@outlook.com>
2018-10-17 16:24:45 -0700

    new submission for Exercise Page 113.

--- Exercise Page 113/Program.cs
+++ Exercise Page 113/Program.cs
@@ -13,6 +13,14 @@ namespace Exercise_Page_113
             state y = new state(4, 5);
 
             state.Operation(3, 4);
+
+            state state2 = new state(5, 7);
+
+            //Here is the addition I made since the last submission:
+            state.Operation(x: state2.a, y: state2.b);
+
+            
+
         }
     }
 }
