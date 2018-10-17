8af27763cda67f2a393b0c72cf7e60780c0ec32f
GarthAhern <aherng@outlook.com>
2018-10-17 16:08:38 -0700

    Exercise Page 115 Commit

---
+++ Exercise115/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Exercise115
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            var Test1 = point.action1(4);
+            var Test2 = point.action1(4, 3);
+            Console.WriteLine(Test1);
+            Console.WriteLine(Test2);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Exercise115/point.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exercise115
+{
+    class point
+    {
+        public static int action1(int firstInt, int secondInt = 0, string str = "")
+        {
+            firstInt = firstInt * 10 + 7;
+            secondInt = secondInt - 2 * 3;
+            int i = firstInt + secondInt;
+
+            return i;
+        }
+
+    }
+}
